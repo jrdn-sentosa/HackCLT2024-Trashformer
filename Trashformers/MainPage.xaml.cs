86ccd459dd819c8ccb33aeed40b97a84dc7f250d
Jordan Sentosa <jrdn.sentosa@gmail.com>
2025-01-07 19:19:53 -0500

    Trashformer

---
+++ Trashformers/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Camera.MAUI;
+using Microsoft.Maui.Controls.PlatformConfiguration;
+using System.Net;
+namespace Trashformers
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void TakePhoto(object sender, EventArgs e)
+        {
+            FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
+            if (photo != null)
+            {
+                var stream = await photo.OpenReadAsync();
+                myImage.Source = ImageSource.FromStream(() => stream);
+            }
+        }
+
+            private async void Button_Clicked(object sender, EventArgs e)
+        {
+            FileResult photo = MediaPicker.Default.CapturePhotoAsync().Result;
+            myImage.Source = photo.FileName;
+            
+        }
+    }
+}
---
+++ Trashformers/obj/Debug/net7.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("Trashformers.MainApplication, Trashformers, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc643306c147e60539da.MainApplication.class, crc643306c147e60539da.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
